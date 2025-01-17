bef4eb0e5efb74f749e08525fadbe8338d808cb6
佐々木 大 <dai-sasaki@zyyx.jp>
2025-01-17 17:03:24 +0900

    no message

--- src/main.js
+++ src/main.js
@@ -4,3 +4,13 @@ import './registerServiceWorker'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker登録成功:', registration);
+      })
+      .catch(error => {
+        console.error('Service Worker登録失敗:', error);
+      });
+  }

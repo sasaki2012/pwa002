39cfc44da0e23bbf2dc0098be1dc16d00dbc1fed
佐々木 大 <dai-sasaki@zyyx.jp>
2025-01-17 17:56:51 +0900

    no message

--- src/main.js
+++ src/main.js
@@ -3,14 +3,4 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('Service Worker登録成功:', registration);
-      })
-      .catch(error => {
-        console.error('Service Worker登録失敗:', error);
-      });
-  }
-
-  createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount('#app')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,63 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button id="confirm">通知の許可を取得する</button>
+    <!-- <button id="send">通知を送信する</button> -->
   </div>
 </template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  mounted() {
+    // 通知の許可
+    const confirmButton = document.getElementById("confirm");
+    if (confirmButton) {
+      confirmButton.addEventListener(
+        "click",
+        this.requestNotificationPermission
+      );
+    }
+    // // ServiceWorkerの登録
+    // window.addEventListener("load", () => {
+    //   if ("serviceWorker" in navigator) {
+    //     navigator.serviceWorker
+    //       .register("./service-worker.js")
+    //       .then((registration) => console.log("registered", registration))
+    //       .catch((error) => console.log("error", error));
+    //   }
+    // });
+    // // 通知の送信
+    // document.getElementById("send").addEventListener("click", () => {
+    //   if (Notification.permission === "granted") {
+    //     navigator.serviceWorker
+    //       .register("./service-worker.js")
+    //       .then((registration) => {
+    //         const options = {
+    //           body: "プッシュ通知の本文",
+    //           icon: "",
+    //         };
+    //         registration.showNotification("プッシュ通知のタイトル", options);
+    //       });
+    //   } else {
+    //     alert("通知の許可が必要です");
+    //   }
+    // });
+  },
+  methods: {
+    requestNotificationPermission() {
+      Notification.requestPermission().then((permission) => {
+        console.log(permission);
+        alert(
+          permission === "granted"
+            ? "通知の許可が完了しました。"
+            : "通知の許可が拒否されました。"
+        );
+      });
+    },
+  },
+};
+</script>

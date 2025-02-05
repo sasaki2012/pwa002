e1d22bcc40954bcfb0f571ab0febad595f4bfe01
佐々木 大 <dai-sasaki@zyyx.jp>
2025-02-05 16:42:23 +0900

    6

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,6 +9,8 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 import { initializeApp } from "firebase/app";
 import { getMessaging, getToken } from "firebase/messaging";
+import { firebaseConfig } from "@/config/firebaseConfig";
+
 
 export default {
   name: 'HomeView',
@@ -26,49 +28,45 @@ export default {
   methods: {
     requestNotificationPermission() {
       if ("serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .register("/service-worker.js")
-        .then(() => {
-          console.log("Service worker registered.");
-        })
-        .catch((error) => {
-          console.warn("Service worker error.", error);
-        });
+        navigator.serviceWorker
+          .register("/service-worker.js")
+          .then(() => {
+            console.log("Service worker registered.");
+          })
+          .catch((error) => {
+            console.warn("Service worker error.", error);
+          });
 
-      if (!("Notification" in window)) {
-        alert("このブラウザは通知に対応していません。");
-        return;
-      }
-
-      Notification.requestPermission().then((permission) => {
-        if (permission === "granted") {
-          this.getFCMToken();
-        } else {
-          console.log("通知の許可が拒否されました。");
+        if (!("Notification" in window)) {
+          alert("このブラウザは通知に対応していません。");
+          return;
         }
-      });
 
-      
-    }
+        // すでに許可されている場合は `getFCMToken()` を実行しない
+        if (localStorage.getItem("pushNotificationGranted") === "true") {
+          console.log("プッシュ通知はすでに許可されています。トークン取得をスキップします。");
+          return;
+        }
+
+        Notification.requestPermission().then((permission) => {
+          if (permission === "granted") {
+            // 許可されたらローカルストレージに保存
+            localStorage.setItem("pushNotificationGranted", "true");
+            this.getFCMToken();
+          } else {
+            console.log("通知の許可が拒否されました。");
+          }
+        });
+      }
     },
     getFCMToken() {
-      const firebaseConfig = {
-        apiKey: "AIzaSyA-kpaHoCYnE7bNNGuLlNtikTKkwt97Sz4",
-        authDomain: "push-notification-sample-cc5b4.firebaseapp.com",
-        projectId: "push-notification-sample-cc5b4",
-        storageBucket: "push-notification-sample-cc5b4.firebasestorage.app",
-        messagingSenderId: "689806237170",
-        appId: "1:689806237170:web:c294dbfe6a5d2345989f6b",
-        measurementId: "G-604YNM1BW7",
-      };
-
       // Firebase 初期化
       const app = initializeApp(firebaseConfig);
       const messaging = getMessaging(app);
 
       // FCM トークン取得
       getToken(messaging, { 
-        vapidKey: "BOdwVfQbieXMwPlWEc7pBYDu9chE4gQOVhmaqo15PNk6UQEr1uJ7MAm7agdMUfRmAdlTJ2rKzLu7t9cBAxzpz6M"
+        vapidKey: firebaseConfig.vapidKey
       })
         .then((currentToken) => {
           if (currentToken) {
